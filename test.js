e7d71ed9f6bd01e16a672806e7124f7ab39ad4e3
Kevin Martensson <kevinmartensson@gmail.com>
2015-02-04 20:52:32 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var bufferToVinyl = require('./');
+var concat = require('concat-stream');
+var fs = require('fs');
+var test = require('ava');
+
+test('create vinyl file from buffer', function (t) {
+	var file = fs.readFileSync(__filename, null);
+	t.assert(bufferToVinyl.file(file).contents.length);
+	t.assert(bufferToVinyl.file(file, 'foo').path === 'foo');
+	t.end();
+});
+
+test('create vinyl stream from buffer', function (t) {
+	var file = fs.readFileSync(__filename, null);
+	var stream = bufferToVinyl.stream(file, 'foo');
+
+	stream.pipe(concat(function (files) {
+		t.assert(files[0].contents.length);
+		t.assert(files[0].path === 'foo');
+	}));
+});
